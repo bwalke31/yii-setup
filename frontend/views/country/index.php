baa37995104c35ff3a9b7d7ca84d42315089712d
bwalker32 <bwalke31@depaul.edu>
2024-05-06 08:49:27 -0500

    Added CRUD opperarations for countries

--- frontend/views/country/index.php
+++ frontend/views/country/index.php
@@ -1,26 +1,45 @@
 <?php
-use yii\helpers\Html;
-use yii\widgets\LinkPager;
-?>
-<h1>Countries</h1>
-<table class="table table-bordered table-hover table-striped">
-    <caption>CC: Country Code</caption>
-        <thead>
-            <tr>
-                <th class="col">CC</th>
-                <th class="col">Name</th>
-                <th class="col">Population</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($countries as $country): ?>
-                <tr>
-                    <td class="col"><?= Html::encode($country->code) ?></td>
-                    <td class="col"><?= Html::encode($country->name) ?></td>
-                    <td class="col"><?= Html::encode($country->population) ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
 
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+use frontend\models\Country;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var frontend\models\CountrySearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Countries';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="country-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'code',
+            'name',
+            'population',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Country $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'code' => $model->code]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
